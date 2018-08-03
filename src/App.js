c5ae7c83a2d92bf06d5caef7a07a98d53e2286eb
PanktiParikh <panktip85@gmail.com>
2018-08-03 09:01:14 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -7,8 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div>
+            <text>About me</text>
+            <text>Resume</text>
+            <text>Experience</text>
+            <text>Blog</text>
+            <text>Contact</text>
+          </div>
+          <h1 className="App-title">Hi! it's me Pankti</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
