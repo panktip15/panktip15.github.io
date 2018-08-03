5ebb373811adf41cc06ca4f2d33c54da9ca5299f
PanktiParikh <panktip85@gmail.com>
2018-08-03 13:43:08 -0400

    added paper.js animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,14 +6,18 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <div>
+        <div>
             <text>About me</text>
             <text>Resume</text>
             <text>Experience</text>
             <text>Blog</text>
             <text>Contact</text>
           </div>
-          <h1 className="App-title">Hi! it's me Pankti</h1>
+          <canvas id="myCanvas" />
+          <img id="pankti" src="pankti.jpg" alt="profile" />
+
+          <h1 className="App-title">Hi! It's me Pankti</h1>
+          <h3> I'm a full-stack engineer</h3>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
